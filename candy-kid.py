d322ffdc9c72da171d6c85c886d0eec038b663a5
dendane-jalil <myemail@example.com>
2023-04-25 21:51:03 +0100

    candy kid details  add

--- candy-kid.py
+++ candy-kid.py
@@ -1,4 +1,4 @@
-# 1431. Kids With the Greatest Number of Candies
+#The problem can be found at leet code : 1431. Kids With the Greatest Number of Candies
 # the problem is considered easy 
 
 class Solution:
