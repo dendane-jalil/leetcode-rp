5bc758666c8fec2f5fffeea8b8bb4ad00c7c82b1
dendane-jalil <myemail@example.com>
2023-04-27 22:36:10 +0100

    tow-sum problem

---
+++ two-sum.py
@@ -0,0 +1,13 @@
+# this  is the two sum problem with the a refrehce of
+# 1. Two Sum in leet code , its an easy one  with a
+#  time complexity of O(n) and a space complexity of O(n)
+#  
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}            
+        for i,num in enumerate(nums):
+            n = target - num 
+            if n not in h:
+                h[num] = i
+            else:
+                 return[h[n], i]
