28dca2095b3f9d3e48c54aeae749d25ed1dc2bda
dendane-jalil <myemail@example.com>
2023-04-28 23:22:43 +0100

    two sum problem

--- two-sum.py
+++ two-sum.py
@@ -1,7 +1,7 @@
 # this  is the two sum problem with the a refrehce of
 # 1. Two Sum in leet code , its an easy one  with a
 #  time complexity of O(n) and a space complexity of O(n)
-#  
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         h = {}            
